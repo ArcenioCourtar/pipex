78d621753802480d0fb8f3cf675bb6f8cc17f3ef
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2023-03-14 18:13:48 +0100

    updated .gitignore, main is just test code

--- main.c
+++ main.c
@@ -6,13 +6,24 @@
 /*   By: acourtar <acourtar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 17:01:20 by acourtar          #+#    #+#             */
-/*   Updated: 2023/03/14 17:02:39 by acourtar         ###   ########.fr       */
+/*   Updated: 2023/03/14 18:13:42 by acourtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include <unistd.h>	// pipe(), close(), read()
+#include <fcntl.h>	// open()
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	return (0);
+	int		pipefd[2];
+	char	buffer[10000];
+
+	if (argc != 5)
+		return (0);
+	ft_bzero(buffer, 10000);
+	pipefd[0] = open(argv[1], O_RDONLY);
+	pipefd[1] = open(argv[4], O_WRONLY);
+	read(pipefd[0], buffer, 9999);
+	ft_printf("%s\n", buffer);
 }
